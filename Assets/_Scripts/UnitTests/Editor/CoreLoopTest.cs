245e2dc1012a5aeeea711a353d1fc75754fbcf0b
Risul <risulkarimx1@gmail.com>
2024-03-19 01:47:07 +0600

    added odin, zenject, unitask, newtonsoft json, google spreadsheet api for unity, newtonsoft json converter

---
+++ Assets/_Scripts/UnitTests/Editor/CoreLoopTest.cs
@@ -0,0 +1,15 @@
+using NUnit.Framework;
+using Zenject;
+
+namespace _Scripts.UnitTests.Editor
+{
+    [TestFixture]
+    public class CoreLoopTest : ZenjectUnitTestFixture
+    {
+        [Test]
+        public void TestCoreLoop()
+        {
+            Assert.Pass();
+        }
+    }
+}
